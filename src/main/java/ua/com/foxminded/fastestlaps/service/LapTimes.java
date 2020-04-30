7ed4eff22e287318595e61b262d0a10941838c31
Maksim Khitrov <khitrov.maks@gmail.com>
2020-04-30 18:51:58 +0300

    added validatedata

--- src/main/java/ua/com/foxminded/fastestlaps/service/LapTimes.java
+++ src/main/java/ua/com/foxminded/fastestlaps/service/LapTimes.java
@@ -1,6 +1,5 @@
 package ua.com.foxminded.fastestlaps.service;
 
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
@@ -9,7 +8,9 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import ua.com.foxminded.fastestlaps.exception.ValidationDataException;
 
@@ -17,53 +18,37 @@ public class LapTimes {
     private static String INDENT = " ";
     private static String DASH = "-";
     private static String LINE_END = "\n";
+    private static String UNDERSCORE = "_";
+    private static String DATE_PATTERN = "\\d{4}-\\d{2}-\\d{2}";
+    private static String EXCEPT_DATE_PATTERN = "[A-Z]|(_\\d{2}:\\d{2}:\\d{2}.\\d{3})";
+    private static String EXCEPT_ABBREVIATION_LOG_PATTERN = "(\\d{4}-\\d{2}-\\d{2})|(_\\d{2}:\\d{2}:\\d{2}.\\d{3})";
+    private static String EXCEPT_ABBREVIATION_ABBREVIATIONS_PATTERN = "\\_.*$";
     private FileReader fileReader = new FileReader();
 
     public String showReport(String pathToTimeLogStart, String pathToTimeLogEnd, String pathToAbbreviations)
-            throws ParseException, IOException {
+            throws ParseException {
 
-        validateFilesLenght(pathToTimeLogStart, pathToTimeLogEnd, pathToAbbreviations);
-        validateDates(pathToTimeLogStart, pathToTimeLogEnd);
-        validateAbbreviationsInFiles(pathToTimeLogStart, pathToTimeLogEnd, pathToAbbreviations);
+        validateData(pathToTimeLogStart, pathToTimeLogEnd, pathToAbbreviations);
 
         Map<String, String> timeLogEnd = parseTimeLog(pathToTimeLogEnd);
-
-        validateTimeLog(timeLogEnd);
-
         Map<String, String> timeLogStart = parseTimeLog(pathToTimeLogStart);
-
-        validateTimeLog(timeLogStart);
-
         Map<String, String[]> abbreviations = parseAbbreviations(pathToAbbreviations);
-
-        validateAbbreviationInAbbreviations(abbreviations);
-        validateSecondDelimiterInAbbreviations(abbreviations);
-
         Map<String, String[]> abbreviationsWithIndents = composeIndentAbbreviations(abbreviations);
         Map<String, String> lapTime = composeLapTime(timeLogStart, timeLogEnd);
 
         return composeResult(abbreviationsWithIndents, lapTime);
     }
 
-    private void validateFilesLenght(String pathToTimeLogStart, String pathToTimeLogEnd, String pathToAbbreviations) {
-        int startLogLength = (int) fileReader.parseFile(pathToTimeLogStart).count();
-        int endLogLength = (int) fileReader.parseFile(pathToTimeLogEnd).count();
-        int abbreviationsLength = (int) fileReader.parseFile(pathToAbbreviations).count();
-
-        if (startLogLength != endLogLength && startLogLength != abbreviationsLength) {
-            throw new ValidationDataException("The number of lines in the files should be the same");
-        }
-    }
-
     private Map<String, String> parseTimeLog(String fileName) {
 
-        return fileReader.parseFile(fileName).map(s -> s.replaceAll("\\d{4}-\\d{2}-\\d{2}", ""))
-                .map(s -> s.split("_", 2)).collect(Collectors.toMap(a -> a[0], a -> a[1]));
+        return fileReader.parseFile(fileName).map(s -> s.replaceAll(DATE_PATTERN, "")).map(s -> s.split(UNDERSCORE, 2))
+                .collect(Collectors.toMap(a -> a[0], a -> a[1]));
     }
 
-    private Map<String, String[]> parseAbbreviations(String fileName) throws ValidationDataException, IOException {
+    private Map<String, String[]> parseAbbreviations(String fileName) {
 
-        return fileReader.parseFile(fileName).map(s -> s.split("_", 3)).collect(Collectors.toMap(a -> a[0], a -> a));
+        return fileReader.parseFile(fileName).map(s -> s.split(UNDERSCORE, 3))
+                .collect(Collectors.toMap(a -> a[0], a -> a));
     }
 
     private Map<String, String[]> composeIndentAbbreviations(Map<String, String[]> abbreviations) {
@@ -135,7 +120,7 @@ public class LapTimes {
     }
 
     private String composeResult(Map<String, String[]> abbreviations, Map<String, String> lapTime) {
-        String result = "";
+        StringBuilder result = new StringBuilder("");
         int i = 1;
         int delimiterLine = 16;
         int indentedNumbers = 10;
@@ -143,20 +128,21 @@ public class LapTimes {
 
         for (Map.Entry<String, String> entry : lapTime.entrySet()) {
             if (i == delimiterLine) {
-                result += calculateIndentation(DASH, calculateLengthOfDelimiter(abbreviations)) + LINE_END;
+                result.append(calculateIndentation(DASH, calculateLengthOfDelimiter(abbreviations))).append(LINE_END);
             }
 
             if (i < indentedNumbers) {
-                result += " ";
+                result.append(INDENT);
             }
 
             description = abbreviations.get(entry.getKey());
 
-            result += i + ". " + description[1] + " | " + description[2] + " | " + entry.getValue() + LINE_END;
+            result.append(i).append(". ").append(description[1]).append(" | ").append(description[2]).append(" | ")
+                    .append(entry.getValue()).append(LINE_END);
 
             i++;
         }
-        return result;
+        return result.toString();
     }
 
     private int calculateLengthOfDelimiter(Map<String, String[]> abbreviations2) {
@@ -166,79 +152,64 @@ public class LapTimes {
         return value[1].length() + value[2].length() + 19;
     }
 
-    private void validateTimeLog(Map<String, String> timeLog) {
-        for (Map.Entry<String, String> entry : timeLog.entrySet()) {
-            if (entry.getKey().length() != 3) {
-                throw new ValidationDataException("There is an error in the abbreviation in the end.log file");
-            } else if (!entry.getValue().matches("\\d{2}:\\d{2}:\\d{2}.\\d{3}")) {
-                throw new ValidationDataException("There is an error in the abbreviation in the end.log file");
-            }
+    private void validateData(String pathToTimeLogStart, String pathToTimeLogEnd, String pathToAbbreviations) {
+        Supplier<Stream<String>> startLines = () -> fileReader.parseFile(pathToTimeLogStart);
+        Supplier<Stream<String>> endLines = () -> fileReader.parseFile(pathToTimeLogEnd);
+        Supplier<Stream<String>> abbreviationsLines = () -> fileReader.parseFile(pathToAbbreviations);
+
+        validateFilesLenght(startLines.get(), endLines.get(), abbreviationsLines.get());
+        validateDatesInLogFile(startLines.get());
+        validateDatesInLogFile(endLines.get());
+        validateTimeLogAbbreviationLength(startLines.get());
+        validateTimeLogAbbreviationLength(endLines.get());
+        validateTimeLogDates(startLines.get());
+        validateTimeLogDates(endLines.get());
+        validateDatesInLogs(startLines.get(), endLines.get());
+        validateAbbreviationInAbbreviations(abbreviationsLines.get());
+        validateAbbreviationsInFiles(startLines.get(), endLines.get(), abbreviationsLines.get());
+        validateSecondDelimiterInAbbreviations(abbreviationsLines.get());
+    }
+
+    private void validateFilesLenght(Stream<String> startLines, Stream<String> endLines,
+            Stream<String> abbreviationsLines) {
+        int startLogLength = (int) startLines.count();
+        int endLogLength = (int) endLines.count();
+        int abbreviationsLength = (int) abbreviationsLines.count();
+
+        if (startLogLength != endLogLength || startLogLength != abbreviationsLength) {
+            throw new ValidationDataException("The number of lines in all files should be the same");
         }
     }
 
-    private void validateAbbreviationInAbbreviations(Map<String, String[]> abbreviations) {
-        for (Map.Entry<String, String[]> entry : abbreviations.entrySet()) {
-            if (entry.getKey().length() != 3) {
-                throw new ValidationDataException(
-                        "There is an error in the abbreviation in the abbreviations.txt file");
-
-            }
-        }
-
-    }
-
-    private void validateSecondDelimiterInAbbreviations(Map<String, String[]> abbreviations) {
-        String[] description;
-
-        for (Map.Entry<String, String[]> entry : abbreviations.entrySet()) {
-            description = abbreviations.get(entry.getKey());
-
-            if (description.length < 3) {
-                throw new ValidationDataException(
-                        "There is an error in the abbreviation in the abbreviations.txt file");
-            }
-
-        }
-    }
-
-    private void validateDates(String pathToTimeLogStart, String pathToTimeLogEnd) {
-        List<String> startLog;
-        List<String> endLog;
-
-        if (!fileReader.parseFile(pathToTimeLogStart).allMatch(line -> line.matches("(.*)\\d{4}-\\d{2}-\\d{2}(.*)"))) {
+    private void validateDatesInLogFile(Stream<String> logFileLines) {
+        if (!logFileLines.allMatch(line -> line.matches("(.*)" + DATE_PATTERN + "(.*)"))) {
             throw new ValidationDataException("Dates in end.log and start.log files do not match");
-        } else if (!fileReader.parseFile(pathToTimeLogEnd)
-                .allMatch(line -> line.matches("(.*)\\d{4}-\\d{2}-\\d{2}(.*)"))) {
-            throw new ValidationDataException("Dates in end.log and start.log files do not match");
-        } else {
-            startLog = fileReader.parseFile(pathToTimeLogStart)
-                    .map(s -> s.replaceAll("[A-Z]|(_\\d{2}:\\d{2}:\\d{2}.\\d{3})", "")).collect(Collectors.toList());
-            endLog = fileReader.parseFile(pathToTimeLogEnd)
-                    .map(s -> s.replaceAll("[A-Z]|(_\\d{2}:\\d{2}:\\d{2}.\\d{3})", "")).collect(Collectors.toList());
-            int i = 0;
-            for (String line : startLog) {
-                if (line.equals(endLog.get(i))) {
-                    i++;
-                } else {
-                    throw new ValidationDataException("Dates in end.log and start.log files do not match");
-                }
-            }
         }
     }
 
-    private void validateAbbreviationsInFiles(String pathToTimeLogStart, String pathToTimeLogEnd,
-            String pathToAbbreviations) {
-        List<String> abbreviationsFromStartLog;
-        List<String> abbreviationsFromEndLog;
-        List<String> abbreviationsFromTxt;
+    private void validateDatesInLogs(Stream<String> startLines, Stream<String> endLines) {
+        List<String> startLog = startLines.map(s -> s.replaceAll(EXCEPT_DATE_PATTERN, "")).collect(Collectors.toList());
+        List<String> endLog = endLines.map(s -> s.replaceAll(EXCEPT_DATE_PATTERN, "")).collect(Collectors.toList());
+        int i = 0;
 
-        abbreviationsFromStartLog = fileReader.parseFile(pathToTimeLogStart)
-                .map(line -> line.replaceAll("(\\d{4}-\\d{2}-\\d{2})|(_\\d{2}:\\d{2}:\\d{2}.\\d{3})", ""))
-                .collect(Collectors.toList());
-        abbreviationsFromEndLog = fileReader.parseFile(pathToTimeLogEnd)
-                .map(line -> line.replaceAll("(\\d{4}-\\d{2}-\\d{2})|(_\\d{2}:\\d{2}:\\d{2}.\\d{3})", ""))
-                .collect(Collectors.toList());
-        abbreviationsFromTxt = fileReader.parseFile(pathToAbbreviations).map(line -> line.replaceAll("\\_.*$", ""))
+        for (String line : startLog) {
+            if (line.equals(endLog.get(i))) {
+                i++;
+            } else {
+                throw new ValidationDataException("Dates in end.log and start.log files do not match");
+            }
+        }
+
+    }
+
+    private void validateAbbreviationsInFiles(Stream<String> startLines, Stream<String> endLines,
+            Stream<String> abbreviationsLines) {
+        List<String> abbreviationsFromStartLog = startLines
+                .map(line -> line.replaceAll(EXCEPT_ABBREVIATION_LOG_PATTERN, "")).collect(Collectors.toList());
+        List<String> abbreviationsFromEndLog = endLines
+                .map(line -> line.replaceAll(EXCEPT_ABBREVIATION_LOG_PATTERN, "")).collect(Collectors.toList());
+        List<String> abbreviationsFromTxt = abbreviationsLines
+                .map(line -> line.replaceAll(EXCEPT_ABBREVIATION_ABBREVIATIONS_PATTERN, ""))
                 .collect(Collectors.toList());
         Collections.sort(abbreviationsFromStartLog);
         Collections.sort(abbreviationsFromEndLog);
@@ -250,6 +221,55 @@ public class LapTimes {
                 i++;
             } else {
                 throw new ValidationDataException("There is an error in the abbreviation in files");
+            }
+        }
+    }
+
+    private void validateTimeLogAbbreviationLength(Stream<String> logFileLines) {
+        List<String> timeLog = logFileLines.map(s -> s.replaceAll(EXCEPT_ABBREVIATION_LOG_PATTERN, ""))
+                .collect(Collectors.toList());
+
+        for (String line : timeLog) {
+            if (line.length() != 3) {
+                throw new ValidationDataException("There is an error in the abbreviation in the end.log file");
+            }
+        }
+    }
+
+    private void validateTimeLogDates(Stream<String> logFileLines) {
+        Map<String, String> timeLog = logFileLines.map(s -> s.replaceAll(DATE_PATTERN, ""))
+                .map(s -> s.split(UNDERSCORE, 2)).collect(Collectors.toMap(a -> a[0], a -> a[1]));
+
+        for (Map.Entry<String, String> entry : timeLog.entrySet()) {
+            if (!entry.getValue().matches("\\d{2}:\\d{2}:\\d{2}.\\d{3}")) {
+                throw new ValidationDataException("There is an error in the abbreviation in the end.log file");
+            }
+        }
+    }
+    
+    private void validateAbbreviationInAbbreviations(Stream<String> abbreviationsLines) {
+        List<String> parseAbbreviations = abbreviationsLines
+                .map(line -> line.replaceAll(EXCEPT_ABBREVIATION_ABBREVIATIONS_PATTERN, ""))
+                .collect(Collectors.toList());
+
+        for (String line : parseAbbreviations) {
+            if (line.length() != 3) {
+                throw new ValidationDataException("There is an error in the abbreviation in the abbreviations.txt file");
+            }
+        }
+    }
+
+    private void validateSecondDelimiterInAbbreviations(Stream<String> abbreviationsLines) {
+        Map<String, String[]> abbreviations = abbreviationsLines.map(s -> s.split(UNDERSCORE, 3))
+                .collect(Collectors.toMap(a -> a[0], a -> a));
+        String[] description;
+
+        for (Map.Entry<String, String[]> entry : abbreviations.entrySet()) {
+            description = abbreviations.get(entry.getKey());
+
+            if (description.length < 3) {
+                throw new ValidationDataException(
+                        "Missing second delimiter in the abbreviations.txt file");
             }
         }
     }
