89fa684dbd8be3982de1bc3580903d7dd326209a
Maksim Khitrov <khitrov.maks@gmail.com>
2020-04-30 18:52:50 +0300

    refactoring runner

--- src/main/java/ua/com/foxminded/fastestlaps/Runner.java
+++ src/main/java/ua/com/foxminded/fastestlaps/Runner.java
@@ -1,13 +1,12 @@
 package ua.com.foxminded.fastestlaps;
 
-import java.io.IOException;
 import java.text.ParseException;
 
 import ua.com.foxminded.fastestlaps.service.LapTimes;
 
 public class Runner {
 
-    public static void main(String[] args) throws ParseException, IOException {
+    public static void main(String[] args) throws ParseException {
 
         String pathToTimeLogStart = "start.log";
         String pathToTimeLogEnd = "end.log";
