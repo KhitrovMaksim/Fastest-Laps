f81f4edd2e69bef3e2c02dbaa69956cb6b88eb25
Maksim Khitrov <khitrov.maks@gmail.com>
2020-04-30 18:50:12 +0300

    filereadertest refactoring

--- src/test/java/ua/com/foxminded/fastestlaps/FileReaderTest.java
+++ src/test/java/ua/com/foxminded/fastestlaps/FileReaderTest.java
@@ -2,9 +2,6 @@ package ua.com.foxminded.fastestlaps;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.io.IOException;
-import java.text.ParseException;
-
 import org.junit.jupiter.api.Test;
 
 import ua.com.foxminded.fastestlaps.exception.ValidationDataException;
@@ -12,91 +9,85 @@ import ua.com.foxminded.fastestlaps.service.FileReader;
 
 public class FileReaderTest {
     FileReader fileReader = new FileReader();
-    
+
     @Test
     void fileReader_ThrowsValidationException_IfStartLogFileIsNotFound() {
-        String pathToTimeLogStart = "start.log";
+        String pathToTimeLogStart = "startNotExist.log";
 
         assertThrows(ValidationDataException.class, () -> {
             fileReader.parseFile(pathToTimeLogStart);
         });
     }
-    
+
     @Test
     void fileReader_ThrowsValidationException_IfEndLogFileIsNotFound() {
-        String pathToTimeLogEnd = "end.log";
+        String pathToTimeLogEnd = "endNotExist.log";
 
         assertThrows(ValidationDataException.class, () -> {
             fileReader.parseFile(pathToTimeLogEnd);
         });
     }
-    
+
     @Test
     void fileReader_ThrowsValidationException_IfAbbreviationFilesIsNotFound() {
-        String pathToabbreviations = "abbreviations.txt";
+        String pathToabbreviations = "abbreviationsNotExist.txt";
 
         assertThrows(ValidationDataException.class, () -> {
             fileReader.parseFile(pathToabbreviations);
         });
     }
-    
+
     @Test
-    void fileReader_ThrowsValidationExceptionWithMessage_IfInAbbreviationsBlankLine() throws ValidationDataException, ParseException {
-        String pathToabbreviations = "src\\test\\resources\\abbreviationsWithBlankLine.txt";
-        String message = "Blank line in " + pathToabbreviations;
-        
+    void fileReader_ThrowsValidationException_IfInAbbreviationsBlankLine() {
+        String pathToabbreviations = "abbreviationsWithBlankLine.txt";
+
         assertThrows(ValidationDataException.class, () -> {
             fileReader.parseFile(pathToabbreviations);
-        }, message);
-    }
-    
-    @Test
-    void fileReader_ThrowsValidationExceptionWithMessage_IfInStartLogBlankLine() throws ValidationDataException, ParseException {
-        String pathToTimeLogStart = "src\\test\\resources\\startWithBlankLine.log";
-        String message = "Blank line in " + pathToTimeLogStart;
-        
-        assertThrows(ValidationDataException.class, () -> {
-            fileReader.parseFile(pathToTimeLogStart);
-        }, message);
-    }
-    
-    @Test
-    void fileReader_ThrowsValidationExceptionWithMessage_IfInEndLogBlankLine() throws ValidationDataException, ParseException {
-        String pathToTimeLogEnd = "src\\test\\resources\\endWithBlankLine.log";
-        String message = "Blank line in " + pathToTimeLogEnd;
-        
-        assertThrows(ValidationDataException.class, () -> {
-            fileReader.parseFile(pathToTimeLogEnd);
-        }, message);
+        });
     }
 
     @Test
-    void fileReader_ThrowsValidationExceptionWithMessage_IfInEndLogDeletedDelimiter() throws IOException, ParseException {
-        String pathToTimeLogEnd = "src\\test\\resources\\endWithoutDelimiter.log";
-        String message = "Missing delimiter in " + pathToTimeLogEnd;
-        
+    void fileReader_ThrowsValidationException_IfInStartLogBlankLine() {
+        String pathToTimeLogStart = "startWithBlankLine.log";
+
         assertThrows(ValidationDataException.class, () -> {
-            fileReader.parseFile(pathToTimeLogEnd);
-        }, message);
+            fileReader.parseFile(pathToTimeLogStart);
+        });
     }
 
     @Test
-    void fileReader_ThrowsValidationExceptionWithMessage_IfInStartLogDeletedDelimiter() throws IOException, ParseException {
-        String pathToTimeLogStart = "src\\test\\resources\\startWithoutDelimiter.log";
-        String message = "Missing delimiter in " + pathToTimeLogStart;
-        
+    void fileReader_ThrowsValidationException_IfInEndLogBlankLine() {
+        String pathToTimeLogEnd = "endWithBlankLine.log";
+
+        assertThrows(ValidationDataException.class, () -> {
+            fileReader.parseFile(pathToTimeLogEnd);
+        });
+    }
+
+    @Test
+    void fileReader_ThrowsValidationException_IfInEndLogDeletedDelimiter() {
+        String pathToTimeLogEnd = "endWithoutDelimiter.log";
+
+        assertThrows(ValidationDataException.class, () -> {
+            fileReader.parseFile(pathToTimeLogEnd);
+        });
+    }
+
+    @Test
+    void fileReader_ThrowsValidationException_IfInStartLogDeletedDelimiter() {
+        String pathToTimeLogStart = "startWithoutDelimiter.log";
+
         assertThrows(ValidationDataException.class, () -> {
             fileReader.parseFile(pathToTimeLogStart);
-        }, message);
+        });
     }
-    
+
     @Test
-    void fileReader_ThrowsValidationExceptionWithMessage_IfInAbbreviationsDeletedTwoDelimiters() throws IOException, ParseException {
-        String pathToabbreviations = "src\\test\\resources\\abbreviationsWithoutTwoDelimiters.txt";
-        String message = "Missing delimiter in " + pathToabbreviations;
-        
+    void fileReader_ThrowsValidationException_IfInAbbreviationsDeletedTwoDelimiters() {
+        String pathToabbreviations = "abbreviationsWithoutTwoDelimiters.txt";
+
         assertThrows(ValidationDataException.class, () -> {
             fileReader.parseFile(pathToabbreviations);
-        }, message);
+        });
     }
 }
